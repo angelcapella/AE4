2a44fab2004fa5c8738860f6a6a8d5e97f7495d8
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-11-30 09:57:11 +0100

    1.1

--- scr/js/main.js
+++ scr/js/main.js
@@ -1,44 +1,17 @@
 document.addEventListener('DOMContentLoaded' , () => {
     
-const miLienzo = document.getElementById('miGrafica')
-const contexto = miLienzo.getContext('2d');
-// const configuracion= {
-//     type:'line',
-//     data:{
-//         label: [1,2,3,4,5,6,7,8,9,10,11,12],
-//         datasets:[{
-//             label: 'Temperaturas',
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [ 3, 4, 3, 5, 7, 10, 12, 15, 12, 10,7, 5],
-//         }],
-//         datasets:[{
-//             label: 'Lluvias',
-//             backgroundColor: 'rgb(255, 100, 13)',
-//             borderColor: 'rgb(255, 100, 13)',
-//             data: [ 40 , 60 , 110 , 70 , 20 , 15 , 10, 15 , 90 , 120 , 80 , 50 ],
-//         }],
-//     },
-   
-// };
-// const grafica = new Chart(contexto, configuracion);
-
-
-
-
-
 Chart.defaults.global.defaultFontFamily = "Lato";
 Chart.defaults.global.defaultFontSize = 18;
 
-var serie1 = {
+const serie1 = {
     label: "Temperaturas",
-    data: [  3, 4, 3, 5, 7, 10, 12, 15, 12, 10,7, 5],
+    data: [3, 4, 3, 5, 7, 10, 12, 15, 12, 10,7, 5],
     lineTension: 0,
     fill: false,
     borderColor: 'red'
   };
 
-var serie2 = {
+const serie2 = {
     label: "Lluvias",
     data: [40 , 60 , 110 , 70 , 20 , 15 , 10, 15 , 90 , 120 , 80 , 50 ],
     lineTension: 0,
@@ -46,12 +19,12 @@ var serie2 = {
   borderColor: 'blue'
   };
 
-var tiempo = {
+const tiempo = {
   labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
   datasets: [serie1, serie2]
 };
 
-var chartOptions = {
+const chartOptions = {
   legend: {
     display: true,
     position: 'top',
@@ -62,7 +35,7 @@ var chartOptions = {
   }
 };
 
-var lineChart = new Chart(miGrafica, {
+const lineChart = new Chart(miGrafica, {
   type: 'bar',
   data: tiempo,
   options: chartOptions
