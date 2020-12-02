fbb5454591d38bf1984b9d75de321cd299f36e07
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-12-02 23:06:10 +0100

    1.4

--- scr/js/main.js
+++ scr/js/main.js
@@ -1,63 +1,51 @@
 document.addEventListener('DOMContentLoaded' , () => {
     
-
+const Grafica = document.getElementById('miGrafica');
+const contexto = Grafica.getContext('2d');
 Chart.defaults.global.defaultFontSize = 18;
 
-const serie1 = {
-    label: "Temperaturas",
+const configuracion = new Chart(contexto, {
+type:'bar',
+data:{
+    datasets:[{
+    label: 'Lluvias',
     data: [3, 4, 3, 5, 7, 10, 12, 15, 12, 10, 7, 5],
-    borderColor: 'red'
-  };
+    borderColor:'blue'
+    
+    },{
+      label: 'Temperatura',
+      data: [40, 60, 110, 70, 20, 15, 10, 15, 90, 120, 80, 50 ],
+      borderColor:'red'
+  }],
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+  },
 
-const serie2 = {
-    label: "Lluvias",
-    data: [40, 60, 110, 70, 20, 15, 10, 15, 90, 120, 80, 50 ],
-    borderColor: 'blue'
-  };
+});
+  const grafica = new Chart(contexto, configuracion);
 
-const Tiempo = {
-  labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
-  datasets: [serie1, serie2]
-};
 
-const chartOptions = {
-  legend: {
-    display: true,
-    position: 'top',
-    labels: {
-      boxWidth: 80,
-      fontColor: 'black'
-    }
-  }
-};
 
-const lineChart = {
-  type: 'bar',
-  data: Tiempo,
-  options: chartOptions
-  };
-
-function DataInputs(inputTiempo, inputTemperatura , inputLluvia) {
-  return inputTiempo[0] != "" && inputTemperatura[0] != "" && inputLluvia[0] != "" && inputTiempo.length === inputTemperatura.length && inputTiempo.length === inputLluvia.length ;
+function DataInputs(formTiempo, formTemperatura , formLluvia) {
+  return formTiempo[0] != "" && formTemperatura[0] != "" && formLluvia[0] != "" && formTiempo.length === formTemperatura.length && formTiempo.length === formLluvia.length ;
 }
 
 const miGrafica = document.getElementById('miGrafica');
 const button = document.getElementById('Actualizar');
 button.addEventListener('click', () => {
-  button.value = "actualizar";
-  let inputTiempo = document.getElementById('Tiempo').value.split(',');
-  let inputTemperatura = document.getElementById('Temperatura').value.split(',');
-  let inputLluvia = document.getElementById('Lluvia').value.split(',');
+  button.value = "Actualizar";
+  let formTiempo = document.getElementById('Tiempo').value.split(',');
+  let formTemperatura = document.getElementById('Temperatura').value.split(',');
+  let formLluvia = document.getElementById('Lluvia').value.split(',');
 
-  if (DataInputs (inputTiempo, inputTemperatura, inputLluvia)) {
-    for (let i = 0; i < inputTiempo.length; i++) {
-      chart.data.datasets[0].data[inputTiempo[i] - 1] = inputTiempo[i];
-      chart.data.datasets[1].data[inputTiempo[i] - 1] = inputLluvia[i];
+  if (DataInputs (formTiempo, formTemperatura, formLluvia)) {
+    for (let i = 0; i < formTiempo.length; i++) {
+      chart.data.datasets[0].data[formTiempo[i] - 1] = formTiempo[i];
+      chart.data.datasets[1].data[formTiempo[i] - 1] = formLluvia[i];
       
     }
     chart.update();
     miGrafica.style.display = "block";
-    
+
   }else{
     alert('Rellenar datos');
   }
